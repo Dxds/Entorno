09814f9d233e585adbd6c1204b667a905525ee05
Dxds <86174854+Dxds@users.noreply.github.com>
2021-06-20 07:38:33 -0400

    Update EnvHck.sh

--- EnvHck.sh
+++ EnvHck.sh
@@ -149,7 +149,7 @@ echo "Instalando Dependencias"
 echo $PassWD|sudo -S apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
 echo $PassWD|sudo -S apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 echo "Instalando Polybar"
-echo $PassWD|sudo -S sudo apt install polybar
+echo $PassWD|sudo -S sudo apt install polybar -y
 echo "Instalando polybar-themes"
 echo "clonando repositorio"
 echo $PassWD|sudo -S cp ${PathSt}/Bspwm/bspwm.desktop /usr/share/xsessions/ 
