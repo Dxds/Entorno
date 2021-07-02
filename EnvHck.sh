3c27cd1f9682c570738b881fb57afdad1ae9413b
Dxds <86174854+Dxds@users.noreply.github.com>
2021-07-02 00:57:36 -0400

    Update EnvHck.sh

--- EnvHck.sh
+++ EnvHck.sh
@@ -412,6 +412,8 @@ figlet -c Sublime
 echo $PassWD|sudo -S wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo $PassWD|sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 figlet -c Instalacion ROOT
 echo $PassWD|sudo -S su - root -c "${PathSt}/EnvHckRoot.sh $User $PathSt"
 clear
